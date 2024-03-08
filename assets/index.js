c1806e6325b229c1090e54b4e80e9db7e17c784e
coolguy-codin-Rydham <rydhampreetsingh.gindra@gmail.com>
2024-03-08 21:25:35 +0530

    Minor upgrades

--- assets/index.js
+++ assets/index.js
@@ -3,9 +3,26 @@ let nav2 = document.getElementById("navagain")
 let cross = document.getElementById("cross")
 ham.addEventListener('click',()=>{
     nav2.classList.toggle("hidden")
-
 })
 
 cross.addEventListener('click',()=>{
+    nav2.classList.toggle("hidden")
+})
+
+let hero = document.getElementById("heroTo")
+let part2 = document.getElementById("part2to")
+let part3 = document.getElementById("part3to")
+let contact = document.getElementById("contactTo")
+
+hero.addEventListener('click',()=>{
+    nav2.classList.toggle("hidden")
+})
+part2.addEventListener('click',()=>{
+    nav2.classList.toggle("hidden")
+})
+part3.addEventListener('click',()=>{
+    nav2.classList.toggle("hidden")
+})
+contact.addEventListener('click',()=>{
     nav2.classList.toggle("hidden")
 })
